06feae971507b48f77965590ff5e2460156e1ca5
GwadaPuk <gwadapuk@gmail.com>
2025-05-09 15:59:21 +0200

    added comparison with upgraded qlearning function

--- stats/main_stat.py
+++ stats/main_stat.py
@@ -1,4 +1,5 @@
 from Algorithms.Q_learning import training
+from Algorithms.APF import create_Q
 from Game_Engine.grid import Grid
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
@@ -11,7 +12,7 @@ def comparison(rows=100, cols=100, nb_training=25000, alphas=[0.1], gammas=[0.9]
         for gamma in gammas:
             print(f'\r Comparaison {alphas.index(alpha)*len(gammas) + gammas.index(gamma) + 1}/{len(alphas)*len(gammas)} :      ', end='')
 
-            Q, step_list = training(grid=G, num_training=nb_training, stats=True, alpha=alpha, gamma=gamma)
+            step_list, reward_list = training(grid=G, num_training=nb_training, stats=True, alpha=alpha, gamma=gamma)
 
             if step_list[-1] < min_length:
                 print(f'\r Redémarrage, longueur insuffisante ({step_list[-1]})')
@@ -72,3 +73,76 @@ gammas = [0.7, 0.9, 0.99]
 colors = ['r', 'g', 'b']
 
 comparison(alphas=alphas, gammas=gammas)
+
+
+def comparison_apf(rows=100, cols=100, nb_training=2500, alpha=0.9, gamma=0.999, min_length=100):
+    G = Grid(rows=100, cols=100, proba=0.2)
+
+    print(f'\r Comparaison Q-Learning :      ', end='')
+    step_list, reward_list = training(grid=G, num_training=nb_training, stats=True, alpha=alpha, gamma=gamma)
+
+    if step_list[-1] < min_length:
+        print(f'\r Redémarrage, longueur insuffisante ({step_list[-1]})')
+        comparison_apf(rows, cols, nb_training, alpha, gamma, min_length)
+        exit()
+
+    APF = (1, 3, 3, 1)
+    print(f'\r Calcul de l\'APF', end='')
+    G.apf_add(APF)
+    Q_APF = create_Q(G, APF[0], APF[1], APF[2], APF[3])
+    print(f'\r Comparaison Q-Learning x APF :      ', end='')
+    step_list_, reward_list_ = training(grid=G, num_training=nb_training, stats=True, alpha=alpha, gamma=gamma, init_matrix=Q_APF)
+
+    x = []
+    y = []
+    z = []
+    average_reward = []
+    average_reward_ = []
+    for i in range(1, nb_training//100):
+        x.append(i)
+        window = step_list[i * 100 - 50: i * 100 + 51]
+        window_ = step_list_[i * 100 - 50: i * 100 + 51]
+
+        average = sum(window) / len(window)
+        average_ = sum(window_) / len(window_)
+
+        y.append(average)
+        z.append(average_)
+
+        window = reward_list[i * 100 - 50: i * 100 + 51]
+        window_ = reward_list_[i * 100 - 50: i * 100 + 51]
+
+        average = sum(window) / len(window)
+        average_ = sum(window_) / len(window_)
+
+        average_reward.append(average)
+        average_reward_.append(average_)
+
+
+    plt.plot(x, y, color='r', label='Q-Learning')
+    plt.plot(x, z, color='b', label='Q-Learning x APF')
+
+
+    plt.xlabel("Nombre de centaines d'entrainement")
+    plt.ylabel("Longueur moyenne du parcours")
+    plt.title("Comparaison Q-Learning vs Q-Learning x APF")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.legend()
+    plt.show()
+
+
+    plt.figure()
+    plt.plot(average_reward, color='r', label='Q-Learning')
+    plt.plot(average_reward_, color='b', label='Q-Learning x APF')
+
+
+    plt.xlabel("Nombre de centaines d'entrainement")
+    plt.ylabel("Somme des récompenses")
+    plt.title("Comparaison Q-Learning vs Q-Learning x APF")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.legend()
+    plt.show()
+
+comparison_apf()
